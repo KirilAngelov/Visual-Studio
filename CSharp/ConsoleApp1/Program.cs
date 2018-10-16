cf9c545485d6751f2ffeb88282da0af8fd8b208e
KirilAngelov <daimonas@abv.bg>
2018-10-16 21:06:20 +0300

    Just finished some assignments

--- CSharp/ConsoleApp1/Program.cs
+++ CSharp/ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Change");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*',n));
+            }
         }
     }
 }
