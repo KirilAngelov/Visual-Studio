2dd1df409480fcefe1d2a50f7b0e360ae9ad71f1
KirilAngelov <daimonas@abv.bg>
2018-10-18 21:46:49 +0300

    1000 days on Earth done

---
+++ CSharp/1000 Days On Earth/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace _1000_Days_On_Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = Console.ReadLine().Split('-').Select(int.Parse).ToArray();
+            DateTime date = new DateTime(info[2],info[1],info[0]);
+            Console.WriteLine(date.AddDays(999).ToString("dd-MM-yyyy"));
+
+           
+            
+        }
+    }
+}
