46dbb3e1f13b2010fbafd0a7b3ee9a861cdac564
KirilAngelov <daimonas@abv.bg>
2018-10-19 22:09:25 +0300

    Task 2 from exam

---
+++ CSharp/Exam2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesKgPrice = double.Parse(Console.ReadLine());
+            double fruitsKgPrice = double.Parse(Console.ReadLine());
+            int vegetablesWeight = int.Parse(Console.ReadLine());
+            int fruitsWeight = int.Parse(Console.ReadLine());
+            double vegetablesPrice = vegetablesKgPrice * vegetablesWeight;
+            double fruitsPrice = fruitsKgPrice * fruitsWeight;
+            double sum = vegetablesPrice + fruitsPrice;
+            Console.WriteLine(sum/1.94);
+
+        }
+    }
+}
