f319e7d5ea953f2f01e19c8c0f1b2990c57ed6de
KirilAngelov <daimonas@abv.bg>
2018-10-19 22:23:58 +0300

    Task 3 from exam

---
+++ CSharp/Exam3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exam3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double tileTime = 0.2;
+            int playgroundSide = int.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileLenght = double.Parse(Console.ReadLine());
+            double benchWidth = double.Parse(Console.ReadLine());
+            double benchLenght = double.Parse(Console.ReadLine());
+            double benchSurface = benchLenght * benchWidth;
+            double playGroudSurface = (playgroundSide * playgroundSide)-benchSurface;
+            double tileSurface = tileLenght * tileWidth;
+            double tilesNumber = (playGroudSurface / tileSurface);
+            double time = tilesNumber * tileTime;
+            Console.WriteLine(tilesNumber);
+            Console.WriteLine(time);
+
+          
+        }
+    }
+}
