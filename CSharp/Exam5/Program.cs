53f7cddf980f47c42de53b04fa2cc816d59aff15
KirilAngelov <daimonas@abv.bg>
2018-10-21 22:14:48 +0300

    Final Tasks

---
+++ CSharp/Exam5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exam5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            double levaFromOneDollar = double.Parse(Console.ReadLine());
+            double moneyFromOneYear = (days * money) * 12 + (days * money) * 2.5;
+          
+            double taxes = ((moneyFromOneYear * 25) / 100);
+            double realMoney = moneyFromOneYear - taxes;
+
+            double levaFromMoney = realMoney * levaFromOneDollar;
+            double levaEveryDay = levaFromMoney / 365;
+            Console.WriteLine(String.Format("{0:0.00}", levaEveryDay));
+        }
+    }
+}
