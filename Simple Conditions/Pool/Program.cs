8c5d4d80e652c0739ac493f4dc78d6e0b60bda9c
KirilAngelov <daimonas@abv.bg>
2018-11-27 21:36:16 +0200

    Tasks

---
+++ Simple Conditions/Pool/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int volumeOfPool = int.Parse(Console.ReadLine());
+            int p1 = int.Parse(Console.ReadLine());
+            int p2 = int.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+            double litresOfp1 = hours * p1;
+            double litresOfp2 = hours * p2;
+            double sum = litresOfp1 + litresOfp2;
+            string percentOfp1= String.Format("{0:0}", (litresOfp1 * 100) / sum)+"%";
+            string percentOfp2 = String.Format("{0:0}", (litresOfp2 * 100) / sum) + "%";
+            string percentOfsum = String.Format("{0:0}", (sum * 100) / volumeOfPool) + "%";
+            if (volumeOfPool>sum)
+            {
+                Console.WriteLine($"The pool is {percentOfsum} full");
+                Console.WriteLine($"Pipe 1:{percentOfp1}");
+                Console.WriteLine($"Pipe 2:{percentOfp2}");
+            }
+            else
+            {
+                double overflow = sum - volumeOfPool;
+                Console.WriteLine($"For {hours} the pool overflows with {overflow} litres");
+            }
+
+
+        }
+    }
+}
---
+++ Simple Conditions/Tom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NoWorkDays = int.Parse(Console.ReadLine());
+            int workDays = 365 - NoWorkDays;
+            int playtime = NoWorkDays * 127 + workDays * 63;
+            if (playtime>30000)
+            {
+                int dif = playtime - 30000;
+                int hours = dif / 60;
+                int minutes = dif - (hours * 60);
+                Console.WriteLine("Tom will rum away");
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+                
+            }
+            else
+            {
+                int dif = 30000 - playtime;
+                int hours = dif / 60;
+                int minutes = dif - (hours * 60);
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+
+            }
+        }
+    }
+}
