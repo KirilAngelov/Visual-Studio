7407a4d8aba463d3107eabe3a37fd5652b71e0c2
KirilAngelov <daimonas@abv.bg>
2018-12-22 17:22:12 +0200

    Assignments

---
+++ Loops/EvenAndOddPosition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EvenAndOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            int sum =0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Equals('a'))
+                {
+                    sum += 1;
+                }
+                if (text[i].Equals('e'))
+                {
+                    sum += 2;
+
+                }
+                if (text[i].Equals('i'))
+                {
+                    sum += 3;
+                }
+                if (text[i].Equals('o'))
+                {
+                    sum += 4;
+                }
+                if (text[i].Equals('u'))
+                {
+                    sum += 5;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
