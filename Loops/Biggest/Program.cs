8673d174e4d03fe1001b3958797358d7c3de6099
KirilAngelov <daimonas@abv.bg>
2018-12-22 16:41:37 +0200

    Assignment

---
+++ Loops/Biggest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Biggest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            for (int i = 0; i <n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                numbers.Add(num);
+            }
+            int biggest = numbers.Max();
+            int lowest = numbers.Min();
+            Console.WriteLine();
+            Console.WriteLine(biggest);
+            Console.WriteLine(lowest);
+        }
+    }
+}
