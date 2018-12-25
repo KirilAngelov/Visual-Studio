4bbf695d6033944d342967de1cc1553ef79c86b2
KirilAngelov <daimonas@abv.bg>
2018-12-25 20:46:01 +0200

    Assignments

---
+++ Loops/Doctors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Doctors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+            int unthreaded = 0;
+            int threaded = 0;
+            int doctors = 7;
+            int nepreglefani = 0;
+            for (int i = 1; i <= period; i++)
+            {
+                int patientsForADay = int.Parse(Console.ReadLine());
+                if (i > 1 && i % 3 == 0)
+                {
+                    if (nepreglefani > threaded)
+                    {
+                        doctors++;
+                    }
+                }
+                if (patientsForADay <= doctors)
+                {
+                    threaded += patientsForADay;
+                }
+                else
+                {
+                    unthreaded = (patientsForADay - doctors);
+                    int patient = patientsForADay - unthreaded;
+                    threaded += patient;
+                    nepreglefani += unthreaded;
+                }
+
+            }
+            Console.WriteLine($"Threaded patients: {threaded}");
+            Console.WriteLine($"Unthreaded patients: {nepreglefani}");
+        }
+    }
+}
---
+++ Loops/Heritage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Heritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ivanAge = 18;
+            double heritage = double.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            for (int i = 1800; i <= year; i++)
+            {
+                if (i%2==0)
+                {
+                    
+                    heritage -= 12000;
+                }
+                else
+                {
+
+                    heritage -= 12000 + ivanAge * 50;
+                    
+                }
+                ivanAge++;
+            }
+            if (heritage>0)
+            {
+                Console.WriteLine($"Yes,he will live a good life and have {String.Format("{0:0.00}",heritage)} dollars left");
+            }
+            else
+            {
+                Console.WriteLine($"No,he will need { String.Format("{0:0.00}", Math.Abs(heritage))} dollars to survive");
+            }
+        }
+    }
+}
