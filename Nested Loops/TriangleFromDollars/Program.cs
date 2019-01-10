6022d0d5b2cac29707bfb61ef41740018b5fa449
KirilAngelov <daimonas@abv.bg>
2019-01-10 22:05:54 +0200

    First assignments from the chapter

---
+++ Nested Loops/TriangleFromDollars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TriangleFromDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("$");
+                for (int j = 0; j < i-1; j++)
+                {
+                    Console.Write(" $");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
