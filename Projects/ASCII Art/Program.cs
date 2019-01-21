b9a3965e2797072697e9a835d3bd8fa512eb9e45
KirilAngelov <daimonas@abv.bg>
2019-01-21 23:54:39 +0200

    Started ASCII Art project and made some progress.

---
+++ Projects/ASCII Art/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ImageMagick;
+using System.Drawing;
+namespace ASCII_Art
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            MagickImageInfo info = new MagickImageInfo("C:\\Users\\spard\\Desktop\\ascii-pineapple.jpg");
+            int width = info.Width;
+            int height = info.Height;
+            Console.WriteLine(width);
+            Console.WriteLine(height);
+            ColorRGB[,] pixel_Matrix = new ColorRGB[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int k = 0; k < height; k++)
+                {
+                   
+                    Pixel pixel= new Pixel(i, k, 4);
+                    pixel_Matrix[i, k] = pixel.ToColor();
+
+
+                }
+            }
+            
+            Console.Write(pixel_Matrix[100,300]);
+
+
+
+
+
+        }
+    }
+}
