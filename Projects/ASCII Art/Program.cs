979fe2da5dd279b7e9ea42ef040eed078f16d0bb
KirilAngelov <daimonas@abv.bg>
2019-02-18 09:49:30 +0200

    Made some code cleanup and added some methods for readaility.

--- Projects/ASCII Art/Program.cs
+++ Projects/ASCII Art/Program.cs
@@ -13,35 +13,30 @@ namespace ASCII_Art
     {
         static void Main(string[] args)
 
-        {
-          
-
-        //Done! Will try to clean up code and document everything.
-        Bitmap image = new Bitmap("C:\\Users\\spard\\Desktop\\Apple.jpg");
+        { 
+            //Done! Will try to clean up code and document everything.
+            Bitmap image = new Bitmap("C:\\Users\\spard\\Desktop\\Apple.jpg");
             Image image1 = new Bitmap("C:\\Users\\spard\\Desktop\\Apple.jpg");
-            Bitmap imageResized=ResizeImage(image1,80,80);
+            Bitmap imageResized = ResizeImage(image1, 60, 60);
             imageResized.RotateFlip(RotateFlipType.Rotate90FlipX);
             int height = imageResized.Height;
             int width = imageResized.Width;
-           char[] characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".ToArray();
-            int[,] pixelMatrix = new int [height+1, width + 1];
+            char[] characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".ToArray();
+            int[,] pixelMatrix = new int[height + 1, width + 1];
             int[,] brightnessMatrix = new int[height + 1, width + 1];
             char[,] asciiMatrix = new char[height + 1, width + 1];
-            
+
             for (int i = 0; i < height; i++)
             {
                 for (int k = 0; k < width; k++)
                 {
-                    Color pixel = imageResized.GetPixel(i,k);
-                  
-                    int redNumber = pixel.R;
-                    int GreenNumber = pixel.G;
-                    int BlueNumber = pixel.B;
-                    int sum = redNumber + BlueNumber + GreenNumber;
+                    Color pixel = imageResized.GetPixel(i, k);
+
+                    int sum = GetSumOfPixels(pixel);
                     pixelMatrix[i, k] = sum;
                     brightnessMatrix[i, k] = sum / 3;
-                    
-                    
+
+
                 }
             }
             Console.WriteLine();
@@ -52,18 +47,22 @@ namespace ASCII_Art
                 {
                     Color pixel = imageResized.GetPixel(i, k);
 
-                    int redNumber = pixel.R;
-                    int GreenNumber = pixel.G;
-                    int BlueNumber = pixel.B;
-                    int sum = (redNumber + BlueNumber + GreenNumber)/3;
+                    int sum = GetSumOfPixels(pixel) / 3;
                     asciiMatrix[i, k] = characters[sum / 4];
                 }
             }
+            PrintAsciiArt(height, width, asciiMatrix);
+
+
+        }
+        //This method prints the final ASCII matrix
+        public static void PrintAsciiArt(int height, int width, char[,] asciiMatrix)
+        {
             for (int i = 0; i < height; i++)
-             {
-                 for (int k = 0; k < width; k++)
-                 {
-                    if (k==width-1)
+            {
+                for (int k = 0; k < width; k++)
+                {
+                    if (k == width - 1)
                     {
                         Console.WriteLine();
                     }
@@ -71,11 +70,18 @@ namespace ASCII_Art
                     {
                         Console.Write(asciiMatrix[i, k]);
                     }
-                    
-                 }
-             }
-            
-           
+
+                }
+            }
+        }
+        //This method gets the sum of each RGB value of the pixel so that we can map it to characters
+        public static int GetSumOfPixels(Color pixel)
+        {
+            int redNumber = pixel.R;
+            int GreenNumber = pixel.G;
+            int BlueNumber = pixel.B;
+            int sum = redNumber + BlueNumber + GreenNumber;
+            return sum;
         }
         public static Bitmap ResizeImage(Image image, int width, int height)
         {
